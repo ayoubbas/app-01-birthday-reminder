3b4cac31fd2f945cf8abf9eac483060f26725259
ayoub <ayoub-ok147@hotmail.fr>
2023-09-03 23:38:03 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import './App.css';
+import "./App.css";
 import data from "./data";
-import Person from './components/person';
-import { useState } from 'react';
-import NoBirth from './components/noBirth';
-
+import Person from "./components/person";
+import { useState } from "react";
+import NoBirth from "./components/noBirth";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { Modal } from "@mui/material";
+import TransitionsModal from "./components/model";
 console.log(data);
 
 function App() {
-  const [myData , setMyData] = useState(data)
+  const [myData, setMyData] = useState(data);
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className='app extraBold'>
-      <div className='container'>
-        <div className='title'>
-          <h1><span>{myData.length}</span> birthdays today</h1>
+    <div className="app extraBold">
+      <AiOutlinePlusCircle
+        onClick={() => {
+          setOpen(!open);
+        }}
+        className="plusIcon"
+      />
+      <TransitionsModal setOpen={setOpen} open={open} />
+      <div className="container">
+        <div className="title">
+          <h1>
+            <span>{myData.length}</span> birthdays today
+          </h1>
         </div>
-          <div className='items'>
-
-          {myData.length !==0 ?myData.map((item)=>{
-            return <Person item={item} key={item.id} />
-          }) : <NoBirth />}
-          </div>
-          <button className='btnDele extraBold' onClick={()=>{setMyData("")}}>Delete  All</button>
+        <div className="items">
+          {myData.length !== 0 ? (
+            myData.map((item) => {
+              return <Person item={item} key={item.id} />;
+            })
+          ) : (
+            <NoBirth />
+          )}
+        </div>
+        <button
+          className="btnDele extraBold"
+          onClick={() => {
+            setMyData("");
+          }}
+        >
+          Delete All
+        </button>
       </div>
     </div>
   );
